0a9dc34d6750748aca6f01ae2a54f173141634b7
jaiver1 <balantajaiver@gmail.com>
2017-11-01 21:50:25 -0500

    FIRST COMMIT

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
-
+'''
 class Maquina(db.Model):
 	id_so = db.Column(db.Integer, primary_key=True)
 	kernel = db.Column(db.String(100), unique=True)
@@ -54,7 +54,7 @@ class Maquina(db.Model):
 
 	def __repr__(self): 
     	return "<Maquina(id='%d', kernel='%s', release='%s', nodename='%s', kernelv='%s', machine='%s', processor='%s', so='%s', hardware='%s', user_logged='%s', cpu_us='%s', cpu_sy='%s',cpu_id='%s', cpu_wa='%s', cpu_st='%s',mem_swpd='%s', mem_free='%s',  mem_buff='%s', cache='%s', swap_si='%s',swap_so='%s')>" % (self.id, self.kernel, self.release, self.nodename, self.kernelv, self.machine, self.processor, self.so, self.hardware, self.user_logged, self.cpu_us, self.cpu_sy, self.cpu_id, self.cpu_wa, self.cpu_st, self.mem_swpd, self.mem_free, self.mem_buff, self.cache, self.swap_si, self.swap_so)
-
+'''
         
 class Descargas(db.Model):
     id_dt = db.Column(db.Integer, primary_key=True)
@@ -73,12 +73,13 @@ class Descargas(db.Model):
 @app.route('/')
 def index():
 	return render_template('index.html')
-     
+
+''' 
 @app.route('/machine/show')
 def machineShow():
 	machine = Maquina.query.filter(Maquina.id == 1).one()
 	return render_template('machine.html',machine = machine)
-
+'''
 
 @app.route('/download/show')
 def downloadShow():
